61f4b147dc8ae0a2882e24af80294d1b96669cb7
relative-rene <rene.arellano415@gmail.com>
2016-08-12 10:26:22 -0700

    adds favicon contact linked

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,17 @@ $(document).ready(function(){
 
 });
 
-  // Show sideNav
-  $('.button-collapse').sideNav('show');
-  // Hide sideNav
-  $('.button-collapse').sideNav('hide');
+
+// Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+
+// Show sideNav
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
