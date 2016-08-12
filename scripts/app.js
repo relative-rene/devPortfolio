1c1c901060c082a7ada1887e46e6c386a87e76f9
relative-rene <rene.arellano415@gmail.com>
2016-08-11 22:53:48 -0700

    adds side navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
-console.log("sanity check");
+$(document).ready(function(){
+  $('.carousel').carousel();
+    $(".button-collapse").sideNav();
+
+});
+
+  // Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
