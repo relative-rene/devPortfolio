8d76fb49d6bb885d54688c3839d179d1a84e8984
relative-rene <rene.arellano415@gmail.com>
2017-07-18 11:39:17 -0700

    adds alternative html

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,16 @@
 console.log('sanity checkt');
-$(document).ready(function(){
-	$('.projectinfo').slick({
-	  dots: false,
-	  infinite: true,
-	  speed: 800,
-	  fade: true,
-	  cssEase: 'linear',
-  	nextArrow: '<i class="slickright fa fa-arrow-circle-right"></i>',
-  	prevArrow: '<i class="slickleft fa fa-arrow-circle-left"></i>',
-  	appendArrows: $('.arrowdiv')
-	});
+$(document).ready(function() {
+  $('.projectinfo').slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    fade: true,
+    cssEase: 'linear',
+    nextArrow: '<i class="slickright fa fa-arrow-circle-right"></i>',
+    prevArrow: '<i class="slickleft fa fa-arrow-circle-left"></i>',
+    appendArrows: $('.arrowdiv')
+  });
+  $('.parallax').parallax();
   $(".button-collapse").sideNav();
 
 });
